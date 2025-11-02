735d4214d539e1b02076666beff2413dfe4ade29
ericmg02 <ericmonne3@gmail.com>
2025-11-03 00:55:23 +0100

    Update entities.py

--- model/entities.py
+++ model/entities.py
@@ -21,7 +21,7 @@ class Piece:
         return self.__cout
     @property
     def ports(self):
-        return self.__self
+        return self.__ports
     @property
     def degre_rarete(self):
         return self.__degre_rarete
@@ -38,7 +38,30 @@ class Piece:
     def obj(self):
         return self.__obj
 
+    #methode
+    def proba_tirage(self): #pour calculer la proba de tirer une piece suivant sa rareté
+        return 1/(3**self.__degre_rarete)
+
 class Inventory:
-    pass
+    def __init__(self):
+        self.objets_consommables = { "pas" : 70, "pieces" : 0, "gemmes" : 2, "cles" : 0, "des" : 0}
+        self.objets_permanents = {
+            "pelle" : False,
+            "marteau" : False,
+            "kit_de_crochetage" : False,
+            "detecteur_de_metaux" : False,
+            "patte_de_lapin" : False}
+
+
+    #methodes
+    #objets consommables
+    def ajouter_conso(self, nom_objet, quantitee):
+        self.objets_consommables[nom_objet] += quantitee
+    def retirer(self, nom_objet, quantitee):
+        if self.objets_consommables[nom_objet] >= quantitee:
+            self.objets_consommables[nom_objet] -= quantitee
+    #objets permanents
+    def ajouter_perm(self, nom_objet):
+        self.objets_permanents[nom_objet] = True
 
     
